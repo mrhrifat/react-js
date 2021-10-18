5b562789874c651c2f6bfc32e7b64925bf0665f3
mrhrifat <mrhrifat383759@gmail.com>
2021-10-19 01:43:12 +0600

    build: lesson 1

---
+++ core/src/components/Lesson1/Lesson1.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+class Lesson1 extends React.Component {
+    render() {
+        return (
+            <div>
+                <h6>Lesson 1</h6>
+
+                {/*
+
+                0.0 Uderstand Core React
+
+                1.1 What is React
+                    
+
+                1.2 How React Works
+                    01) Everything in react is a Component
+                    02) Component is a small piece of UI. Such as, Like, Comment, Share, Button
+                    03) We can resuse component
+                    04) We can use it with other component
+                    05) We can pass data to it from outside
+                    06) It can have it's own data
+                    07) It can be styled seperately
+                    08) It can be used as many as want
+                    09) Relation with one component to another is call component
+                
+                    10) What inside the Component
+                            01) Component has mainly two layer
+                                01) Data Layer (It's Dynamic)
+                                02) Representation Layer (It's Static)
+                    
+                    11) We have to work on data layer. All logic goes here & when data layer will changed, the representation layer will automatically changed.
+
+                    12) How can we use multiple components
+                        01) Representation layer of a component can render other components
+
+
+                1.3. Which type of problem react is solving
+                
+                1.4 Core features of React
+                    01) Components
+                    02) Props & State
+                        01) Props - Data sending
+                        02) State - Change own data
+                    03) Events
+                    04) Conditional renderning
+                    05) List & Tables
+                    06) Forms & Input elements
+                    07) Lifting State Up (Send data child to parent)
+                    08) Composition and Component tree
+
+                1.5 How to use this course
+                    01) Start React
+                    02) Environment setup
+
+                */}
+            </div>
+        )
+    }
+}
+
+export default Lesson1
