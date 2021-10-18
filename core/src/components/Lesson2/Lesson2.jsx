b2c6c7afdf156449511535d4b3e442fd0cb350aa
mrhrifat <mrhrifat383759@gmail.com>
2021-10-19 01:50:12 +0600

    build: lesson 2

---
+++ core/src/components/Lesson2/Lesson2.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+class Lesson2 extends React.Component {
+    render() {
+        return (
+            <div>
+                <h6>Lesson 2</h6>
+                {/* 
+                
+                2.1  Custom environment setup
+
+                2.2 Tools we need to work with React
+                    01) React uses modern JavaScripts (ES6)
+                    02) Some syntax are not relevant
+                    03) Performance optimization
+                    04) Development experience
+                    05) Two main tools we need
+                        01) Babel
+                        02) Webpack
+                
+                2.3 Custom environment setup
+                    01) Go to the website -createapp.dev
+
+                2.4 File structure of react App
+
+                2.5 Our First react app
+                
+                */}
+            </div>
+        )
+    }
+}
+
+export default Lesson2
