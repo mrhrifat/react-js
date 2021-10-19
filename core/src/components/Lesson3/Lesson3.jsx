ea7d7d01504342d01d5b5b724691f07709347a95
mrhrifat <mrhrifat383759@gmail.com>
2021-10-20 01:50:12 +0600

    build: lesson 3

---
+++ core/src/components/Lesson3/Lesson3.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Lesson3 extends Component {
+    render() {
+        return (
+            <div>
+                <h6>Lesson 3</h6>
+
+                {/* 
+                
+                3.1 Create react app
+
+                3.2 Create react app website
+
+                3.3 Create first project
+                    01) pwd
+                    02) mkdir
+                    03) mv oldname newname
+                    04) ls -l
+
+                    05) Code for create app
+                        01) npx create react app {app name}
+                        02) yarn/npm start
+                        03) code .
+
+                3.4 React scripts
+                    01) start - Run project
+                    02) build - After complete project make ready for production
+                    03) test  - To test app
+                    04) eject - 
+                        01) To see default hidden files by React
+                        02) Eject is one way
+                        03) If one time make eject react project, then it can't be return back at first build or created position.
+                        04) It can't be wrap again
+                    
+                3.5 Project file structure
+                    01) yarn/npm run eject
+                    02) Don't keep yarn.lock & npm.lock in one project
+
+                3.6 First react component
+                    01) You can keep extension (.js || .jsx)
+                    02) Where the files contains react component, it's better to use (.jsx) for know characteristics
+                    03) Where the files contains pure JavaScripts, it's better to use (.js) for know characteristics
+
+
+                */}
+
+
+
+            </div>
+        )
+    }
+}
+export default Lesson3
