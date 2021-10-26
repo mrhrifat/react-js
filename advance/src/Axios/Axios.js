aa503bf7cac59cc3a22428c489d46b4c49d40341
mrhrifat <mrhrifat383759@gmail.com>
2021-10-27 01:52:12 +0600

    build: axios

---
+++ advance/src/Axios/Axios.js
@@ -0,0 +1,64 @@
+// import axios from 'axios'
+
+// const url = 'https://jsonplaceholder.typicode.com/users'
+
+// //Success
+// axios.get(url)
+//     .then(res => console.log(res))
+
+// axios.get(url)
+//     .then(res => console.log(res.data))
+
+// const user11 = {
+//     "id": 1,
+//     "name": "Leanne Graham",
+//     "username": "Bret",
+//     "email": "Sincere@april.biz",
+//     "address": {
+//         "street": "Kulas Light",
+//         "suite": "Apt. 556",
+//         "city": "Gwenborough",
+//         "zipcode": "92998-3874",
+//         "geo": {
+//             "lat": "-37.3159",
+//             "lng": "81.1496"
+//         }
+//     }
+// }
+
+// //Success
+
+// axios.post(url, user11)
+//     .then(res => console.log(res))
+
+// axios.post(url, user11)
+//     .then(res => console.log(res.data))
+
+// const user11P = {
+//     "id": 1,
+//     "name": "Ibn Taimiya",
+//     "username": "Bret",
+//     "email": "Sincere@april.biz",
+//     "address": {
+//         "street": "Kulas Light",
+//         "suite": "Apt. 556",
+//         "city": "Gwenborough",
+//         "zipcode": "92998-3874",
+//         "geo": {
+//             "lat": "-37.3159",
+//             "lng": "81.1496"
+//         }
+//     }
+// }
+
+// //Faild
+
+// axios.patch(url, user11P)
+//     .then(res => console.log(res))
+
+// axios.put(url, user11P)
+//     .then(res => console.log(res))
+
+// //Faild
+// axios.delete(url, user11)
+//     .then(res => console.log(res))
