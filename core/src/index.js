ef315a29acea1e3a1747101eafbb4d5b9c0b3010
mrhrifat <mrhrifat383759@gmail.com>
2021-11-22 01:27:12 +0600

    refactor: index js

--- core/src/index.js
+++ core/src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+/*Disbale It*/
+// import "./index.css";
+
+/*Import Sass*/
+// import "./styles/main.style.scss";
+
+/* npm i -D bootstrap jquery popper.js */
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
